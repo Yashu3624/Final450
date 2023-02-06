c37fee15cfa121149605efcc5b8f29d1dfcb5e81
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-02-06 23:27:02 +0530

    Update TripletSuminArray.py

--- TripletSuminArray.py
+++ TripletSuminArray.py
@@ -1,1 +1,7 @@
-
+class Solution:
+     
+    #Function to find if there exists a triplet in the 
+    #array A[] which sums up to X.
+    def find3Numbers(self,A, n, X):
+        # Your Code Here
+        
