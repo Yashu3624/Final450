0b94035c29a1caeb69ffeb8056b180ea36173f53
Yashu3624 <99957916+Yashu3624@users.noreply.github.com>
2023-02-07 06:42:16 +0530

    Update TripletSuminArray.py

--- TripletSuminArray.py
+++ TripletSuminArray.py
@@ -3,5 +3,18 @@ class Solution:
     #Function to find if there exists a triplet in the 
     #array A[] which sums up to X.
     def find3Numbers(self,A, n, X):
+          
         # Your Code Here
+          a = sorted(A)
+          for i in range(n-2):
+               j = i+1
+               k = n-1
+               while(j<k):
+                    if a[i]+a[j]+a[k] == 'X':
+                         return 1
+                    elif a[i]+a[j]+a[k] <= 'X':
+                         j += 1
+                    else:
+                         k -= 1
+          return 0    
         
